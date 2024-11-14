a03a8043b498fbe21833573032b98c7d16002ce7
wicos <wicos@wicos.cn>
2024-11-14 21:09:40 +0800

    add etcd3 support

---
+++ models/etcd.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    :   etcd.py
+@Time    :   2024/11/14 21:06:52
+@Author  :   Wicos 
+@Version :   1.0
+@Contact :   wicos@wicos.cn
+@Blog    :   https://www.wicos.me
+"""
+
+# here put the import lib
